6e2b8bdbb674d4399739635aa2aad879e0b88ac6
Julio Filho <rodriguesjuliocesar145@gmail.com>
2022-08-22 15:19:11 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
     PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
     TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
     PUBLIC_KEY: process.env.NEXT_PUBLIC_KEY,
-    NEXT_PUBLIC_STRAPI_API_URL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
   },
   images: {
     loader: "default",
