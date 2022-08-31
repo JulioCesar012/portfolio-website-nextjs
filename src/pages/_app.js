548e39177d371f29b573746ede44c392b90908b4
JulioCesar012 <rodriguesjuliocesar145@gmail.com>
2022-08-31 12:34:44 -0300

    fix: Corrige configuração do pwa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -31,11 +31,32 @@ function MyApp({ Component, pageProps }) {
         <meta name="msapplication-TileColor" content="#2B5797" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
+
+        <link rel="manifest" href="/manifest.json" />
+
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
         />
 
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://yourdomain.com" />
+        <meta name="twitter:title" content="PWA App" />
+        <meta name="twitter:description" content="Best PWA App in the world" />
+        <meta
+          name="twitter:image"
+          content="https://yourdomain.com/icons/android-chrome-192x192.png"
+        />
+        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="PWA App" />
+        <meta property="og:description" content="Best PWA App in the world" />
+        <meta property="og:site_name" content="PWA App" />
+        <meta property="og:url" content="https://yourdomain.com" />
+        <meta
+          property="og:image"
+          content="https://yourdomain.com/icons/apple-touch-icon.png"
+        />
         <title>Developer Julio</title>
       </Head>
       <GlobalStyle />
