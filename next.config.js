a79472a763ad2674c704b2c562de39300adb3362
JulioCesar012 <rodriguesjuliocesar145@gmail.com>
2022-09-02 10:41:22 -0300

    fix: Corrige pwa config baseado na doc do nextjs

--- next.config.js
+++ next.config.js
@@ -1,29 +1,21 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  disable: process.env.NODE_ENV === "development",
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
-module.exports = withPlugins([
-  {
-    env: {
-      PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
-      TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
-      PUBLIC_KEY: process.env.NEXT_PUBLIC_KEY,
-      NEXT_PUBLIC_STRAPI_API_URL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
-    },
-    images: {
-      loader: "default",
-      domains: ["localhost"],
-    },
+module.exports = withPWA({
+  env: {
+    PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
+    TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
+    PUBLIC_KEY: process.env.NEXT_PUBLIC_KEY,
+    NEXT_PUBLIC_STRAPI_API_URL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
   },
-  [
-    withPWA,
-    {
-      pwa: {
-        disable: process.env.NODE_ENV === "development",
-        dest: "public",
-        register: true,
-        sw: "/sw.js",
-      },
-    },
-  ],
-]);
+  images: {
+    loader: "default",
+    domains: ["localhost"],
+  },
+});
