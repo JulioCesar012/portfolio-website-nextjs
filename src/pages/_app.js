563fabc4df47446b8acba76ad3f82114273eaad2
JulioSilva012 <julio.cesar@ticketandgo.com.br>
2024-07-15 12:10:28 -0300

    :fix: remove tag e script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -64,12 +64,9 @@ function MyApp({ Component, pageProps }) {
           property="og:image"
           content="https://yourdomain.com/icons/apple-touch-icon.png"
         />
-        <meta name="monetag" content="5062c6e81a45694c5ac8c11b697ad352" />
 
         <title>Developer Julio</title>
       </Head>
-      <body>
-      <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7732223"></script>      </body>
       <GlobalStyle />
       <Component {...pageProps} />
     </Layout>
