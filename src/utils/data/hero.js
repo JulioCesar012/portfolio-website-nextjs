5deb2c810b3656fa2bd0637a210434208920cd61
juliocesar012 <devjuliofilho@gmail.com>
2025-12-22 10:10:50 -0300

    :fix: ajusta texto inicial

--- src/utils/data/hero.js
+++ src/utils/data/hero.js
@@ -1,4 +1,4 @@
 export const hero = {
   presentation:
-    "Sou Dev. Frontend/Fullstack. Programo desde os meus 11 anos. Tive oportunidade de desenvolver/trabalhar em projetos bem legais, como o EVA e CONAZ da Ambar Tech. Desenvolvi alguns apps mobile para o seguimento de academia e refrigeração. Mais recentemente topei um desafio grande de criar um App PWA no seguimento delivery, para o banco Itaú. Sou Dev. da Meta D.S.H.",
+    "Sou Dev. Frontend/Fullstack. Programo desde os meus 11 anos. Tive oportunidade de desenvolver/trabalhar em projetos bem legais, como o EVA e CONAZ da Ambar Tech. Desenvolvi alguns apps mobile para o seguimento de academia e refrigeração. Já atuei em grandes clientes da Meta IT, como o Itaú, SInaf, V4Company, DX e 4All",
 };
